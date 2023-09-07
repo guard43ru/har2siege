cb7721050ded385196b21c16f60d85426b115afe
Roman G <guard43ru@gmail.com>
2023-09-07 16:26:38 +0400

    make filter

---
+++ README.md
@@ -0,0 +1,16 @@
+# HAR to Siege converter
+
+Usage:
+```commandline
+python3 ./har2siege.py file.har > output.txt
+```
+Or:
+```commandline
+chmod +x ./har2siege.py
+./har2siege.py file.har > output.txt
+```
+Also available regex filter:
+```commandline
+python3 ./har2siege.py file.har 'https://(www.)?doman.example' > output.txt
+```
+
--- har2siege.py
+++ har2siege.py
@@ -1,20 +1,39 @@
 #!/bin/python3
-# Usage: ./har2siege.py file.har > output.txt
 
 import sys
 import json
+import logging
+import re
+
+logging.basicConfig(format='%(levelname)s: %(message)s')
+
+har = ''
+har_filter = ''
+data = {}
 
 try:
-    arg = sys.argv[1]
-    file = open(arg)
+    har = sys.argv[1]
+except IndexError:
+    logging.error('no HAR file in cli arg')
+    exit(1)
+try:
+    har_filter = sys.argv[2]
+except IndexError:
+    pass
+try:
+    file = open(har)
+except IOError as e:
+    logging.error('%s: %s' % (e.strerror, e.filename))
+    exit(1)
+try:
     data = json.load(file)
     file.close()
-except:
-    print('Error: no HAR file')
-try:
     for key in data['log']['entries']:
-        print(key['request']['method'], key['request']['url'])
-except:
-    print('Error: parsing problem')
-
-
+        if re.search(har_filter, key['request']['url']):
+            if key['request']['method'] == 'POST':
+                print(key['request']['url'], key['request']['method'], key['request']['postData']['text'])
+            elif key['request']['method'] == 'GET':
+                print(key['request']['url'])
+except json.JSONDecodeError as e:
+    logging.error('JSON parsing. %s' % e)
+    exit(1)
